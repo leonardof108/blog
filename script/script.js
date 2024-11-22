083e21cccdf85699c63fd9ca46e67935cd468674
leonardof108 <leonardof108@gmail.com>
2024-11-22 09:13:19 -0300

    added a new page

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const header = document.querySelector("header").appen;
+const header = document.querySelector("header");
 const title = document.createElement("h1");
 
 console.log(header);
